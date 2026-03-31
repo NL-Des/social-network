fc6942247f517dec193111eddd6f10470215e783
Mathis-Pain <mathis.pain76@gmail.com>
2026-03-31 10:43:15 +0200

    Initial Next.js setup with pnpm, src folder, App Router

---
+++ .next/dev/server/chunks/ssr/Desktop_Zone01_social-network__next-internal_server_app_page_actions_0t0uy13.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/Desktop/Zone01/social-network/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=Desktop_Zone01_social-network__next-internal_server_app_page_actions_0t0uy13.js.map
