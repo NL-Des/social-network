6d68e41308379ef70380a17b40137154b2414c61
Mathis-Pain <mathis.pain76@gmail.com>
2026-04-24 11:30:39 +0200

    feat: save profil pic from form register

--- app/auth/login/actions.tsx
+++ app/auth/login/actions.tsx
@@ -1,16 +1,16 @@
-'use server'
+'use server';
 
 export default async function loginAction(formData) {
-  const email = formData.get('email')
-  const password = formData.get('password')
+  const email = formData.get('email');
+  const password = formData.get('password');
 
   // on envoi les données a l'api go
   const response = await fetch('http://localhost:5090/auth/login', {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({email, password})
-  })
-  const data = await response.json()
-  console.log(data.success)
-  return data
+  });
+  const data = await response.json();
+  console.log(data.success);
+  return data;
 }
