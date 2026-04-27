58b0173708262202b334efa748639bd40537cd7f
audoublet <audrey.sommeil@gmail.com>
2026-04-27 11:22:54 +0200

    Creation of structures

---
+++ backend/internal/model/publicProfile.go
@@ -0,0 +1,32 @@
+package model
+
+type PublicProfile struct {
+	ID          int    `json:"id"`
+	Pseudo      string `json:"pseudo"`
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	DateOfBirth string `json:"DateOfBirth"`
+	AboutMe     string `json:"aboutMe"`
+	Avatar      string `json:"avatar"`
+	IsPrivate   bool   `json:"isPrivate"`
+
+	Followers []Follower  `json:"followers"`
+	Following []Following `json:"following"`
+	Posts     []AllPosts  `json:"posts"`
+}
+
+type Follower struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+}
+
+type Following struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+}
+
+type AllPosts struct {
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
