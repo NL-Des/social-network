b99e846a62dae728fbc966982a07b6104b90c3c4
Mathis-Pain <mathis.pain76@gmail.com>
2026-05-11 16:03:11 +0200

    Fix:.next->gitignore

--- .next/dev/static/chunks/Desktop_Zone01_social-network_app_page_tsx_0b9yc2p._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/Desktop_Zone01_social-network_0q4uw_h._.js"],
-    source: "dynamic"
-});
