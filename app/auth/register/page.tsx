ed36eb147210d70d1c06ca58445da049c02f7d7a
Mathis-Pain <mathis.pain76@gmail.com>
2026-05-11 14:38:02 +0200

    Fix: public/images/profil -> gitignore

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -54,7 +54,7 @@ export default function RegisterPage() {
               className="rounded-full text-center h-17"
             />
             <InputStd
-              type="date"
+              type="text"
               placeholder="Date de naissance"
               name="birthday"
               className="rounded-full text-center h-17"
