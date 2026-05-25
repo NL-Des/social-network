fb241f79d5de47a147b49b76dbd198084c172978
Valentine <valentineladjyn@hotmail.fr>
2026-05-25 10:05:55 +0200

    ajout: champ disabled optionnel pour éviter le spam du bouton login et inscription, champ required optionnel pour éviter nom/prénom/datedenaissance vides et les erreurs db qui vont avec

--- app/components/ui/inputStd.tsx
+++ app/components/ui/inputStd.tsx
@@ -1,9 +1,10 @@
-export default function InputStd({type, placeholder, name, className}) {
+export default function InputStd({type, placeholder, name, className, required=false}) {
   return (
     <input
       type={type}
       placeholder={placeholder}
       name={name}
+      required={required}
       className={`border-1 border-brand-border shadow-neon p-5 rounded-2xl text-brand-text ${className ?? ''}`}
     />
   )
